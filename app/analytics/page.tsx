6f5ee43d9671a38d9dea6f0380164abe236666af
Cole Brant <cbrant229@gmail.com>
2024-06-14 03:47:27 -0500

    made the line graph component file disgusting, but it works now

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -3,7 +3,7 @@ import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import { LineGraph } from "@/components/custom/LineGraph";
+import { WeightLineGraph, FatLineGraph, ProteinLineGraph, CarbsLineGraph } from "@/components/custom/LineGraph";
 
 export default async function Home() {
 
@@ -24,9 +24,21 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <h1 className="text-4xl">Weight Progress</h1>
+      <h1 className="text-4xl">Weight Progress</h1>
       <div className="h-[400px] w-[800px]">
-        <LineGraph data={dailyLogs} />
+        <WeightLineGraph data={dailyLogs} key='weight' />
+      </div>
+      <h1 className="text-4xl">Fat Intake</h1>
+      <div className="h-[400px] w-[800px]">
+        <FatLineGraph data={dailyLogs} key='weight' />
+      </div>
+      <h1 className="text-4xl">Protein Intake</h1>
+      <div className="h-[400px] w-[800px]">
+        <ProteinLineGraph data={dailyLogs} key='weight' />
+      </div>
+      <h1 className="text-4xl">Carbs Intake</h1>
+      <div className="h-[400px] w-[800px]">
+        <CarbsLineGraph data={dailyLogs} key='weight' />
       </div>
     </main>
   );
--- components/custom/LineGraph.tsx
+++ components/custom/LineGraph.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { ResponsiveLine } from '@nivo/line'
 
-export const LineGraph = ({ data }:any) => {
-    const formattedData = transformData(data);
+export const WeightLineGraph = ({ data }:any) => {
+
+    const formattedData = transformWeightData(data);
 
     return (
         <ResponsiveLine
@@ -73,9 +74,269 @@ export const LineGraph = ({ data }:any) => {
     );
 };
 
-// Helper function to transform data
-const transformData = (rawData:any) => {
+export const FatLineGraph = ({ data }:any) => {
+
+    const formattedData = transformFatData(data);
+
+    return (
+        <ResponsiveLine
+            data={formattedData}
+            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+            xScale={{ type: 'point' }}
+            yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false
+            }}
+            yFormat=" >-.2f"
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Date',
+                legendOffset: 36,
+                legendPosition: 'middle'
+            }}
+            axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Value',
+                legendOffset: -40,
+                legendPosition: 'middle'
+            }}
+            pointSize={10}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: 'serieColor' }}
+            pointLabel="data.yFormatted"
+            pointLabelYOffset={-12}
+            enableTouchCrosshair={true}
+            useMesh={true}
+            legends={[
+                {
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 100,
+                    translateY: 0,
+                    itemsSpacing: 0,
+                    itemDirection: 'left-to-right',
+                    itemWidth: 80,
+                    itemHeight: 20,
+                    itemOpacity: 0.75,
+                    symbolSize: 12,
+                    symbolShape: 'circle',
+                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemBackground: 'rgba(0, 0, 0, .03)',
+                                itemOpacity: 1
+                            }
+                        }
+                    ]
+                }
+            ]}
+        />
+    );
+};
+
+export const ProteinLineGraph = ({ data }:any) => {
+
+    const formattedData = transformProteinData(data);
+
+    return (
+        <ResponsiveLine
+            data={formattedData}
+            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+            xScale={{ type: 'point' }}
+            yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false
+            }}
+            yFormat=" >-.2f"
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Date',
+                legendOffset: 36,
+                legendPosition: 'middle'
+            }}
+            axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Value',
+                legendOffset: -40,
+                legendPosition: 'middle'
+            }}
+            pointSize={10}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: 'serieColor' }}
+            pointLabel="data.yFormatted"
+            pointLabelYOffset={-12}
+            enableTouchCrosshair={true}
+            useMesh={true}
+            legends={[
+                {
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 100,
+                    translateY: 0,
+                    itemsSpacing: 0,
+                    itemDirection: 'left-to-right',
+                    itemWidth: 80,
+                    itemHeight: 20,
+                    itemOpacity: 0.75,
+                    symbolSize: 12,
+                    symbolShape: 'circle',
+                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemBackground: 'rgba(0, 0, 0, .03)',
+                                itemOpacity: 1
+                            }
+                        }
+                    ]
+                }
+            ]}
+        />
+    );
+};
+
+export const CarbsLineGraph = ({ data }:any) => {
+
+    const formattedData = transformCarbData(data);
+
+    return (
+        <ResponsiveLine
+            data={formattedData}
+            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+            xScale={{ type: 'point' }}
+            yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false
+            }}
+            yFormat=" >-.2f"
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Date',
+                legendOffset: 36,
+                legendPosition: 'middle'
+            }}
+            axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Value',
+                legendOffset: -40,
+                legendPosition: 'middle'
+            }}
+            pointSize={10}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: 'serieColor' }}
+            pointLabel="data.yFormatted"
+            pointLabelYOffset={-12}
+            enableTouchCrosshair={true}
+            useMesh={true}
+            legends={[
+                {
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 100,
+                    translateY: 0,
+                    itemsSpacing: 0,
+                    itemDirection: 'left-to-right',
+                    itemWidth: 80,
+                    itemHeight: 20,
+                    itemOpacity: 0.75,
+                    symbolSize: 12,
+                    symbolShape: 'circle',
+                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemBackground: 'rgba(0, 0, 0, .03)',
+                                itemOpacity: 1
+                            }
+                        }
+                    ]
+                }
+            ]}
+        />
+    );
+};
+
+const transformWeightData = (rawData:any) => {
     const measurements = ['weight'];
+
+    const transformedData = measurements.map(measurement => ({
+        id: measurement,
+        data: rawData.map((entry: { [x: string]: any; date: string | number | Date; }) => ({
+            x: new Date(entry.date).toISOString().split('T')[0], // Extracting just the date part
+            y: entry[measurement]
+        }))
+    }));
+
+    return transformedData;
+};
+
+const transformFatData = (rawData:any) => {
+    const measurements = ['fat'];
+
+    const transformedData = measurements.map(measurement => ({
+        id: measurement,
+        data: rawData.map((entry: { [x: string]: any; date: string | number | Date; }) => ({
+            x: new Date(entry.date).toISOString().split('T')[0], // Extracting just the date part
+            y: entry[measurement]
+        }))
+    }));
+
+    return transformedData;
+};
+
+const transformProteinData = (rawData:any) => {
+    const measurements = ['protein'];
+
+    const transformedData = measurements.map(measurement => ({
+        id: measurement,
+        data: rawData.map((entry: { [x: string]: any; date: string | number | Date; }) => ({
+            x: new Date(entry.date).toISOString().split('T')[0], // Extracting just the date part
+            y: entry[measurement]
+        }))
+    }));
+
+    return transformedData;
+};
+
+const transformCarbData = (rawData:any) => {
+    const measurements = ['carbs'];
 
     const transformedData = measurements.map(measurement => ({
         id: measurement,
