799f307f92b855da2988fa684775d1e0c1a24ccb
Cole Brant <cbrant229@gmail.com>
2024-08-17 00:07:59 -0500

    added loading page at root level of app

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default async function Loading() {
+  return (
+    <main className="flex min-h-screen flex-col p-24 items-center justify-center">
+      <h1 className="text-4xl font-bold">Loading...</h1>
+    </main>
+  );
+}
