5790ff069e6c74d448909dd7a5103443d493fce0
Cole Brant <cbrant229@gmail.com>
2024-08-18 02:48:16 -0500

    breadcrumbs text now changes color to black when scrollY is higher than 200

--- components/custom/PageHeader.tsx
+++ components/custom/PageHeader.tsx
@@ -7,8 +7,10 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
+import { useState, useEffect } from "react";
 
 export const PageHeader = ({ title, description, url }:any) => {
+
   return (
     <>
       <Trail url={url} />
@@ -23,11 +25,33 @@ export const PageHeader = ({ title, description, url }:any) => {
 }
 
 const Trail = ({ url }:any) => {
+
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    // just trigger this so that the initial state 
+    // is updated as soon as the component is mounted
+    // related: https://stackoverflow.com/a/63408216
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const crumbs = url.split('/').filter((crumb:any) => crumb)
 
   return (
     <Breadcrumb className="fixed top-14 left-5">
-      <BreadcrumbList className="text-white">
+      {scrollY > 200 ? (
+        <BreadcrumbList className="text-black">
         {crumbs.map((crumb:any, index:number) => (
           crumb === crumbs[crumbs.length - 1] ? null :
           <BreadcrumbItem key={index}>
@@ -37,10 +61,26 @@ const Trail = ({ url }:any) => {
             <BreadcrumbSeparator/>
           </BreadcrumbItem>
         ))}
-        <BreadcrumbPage className="text-white">
+        <BreadcrumbPage className="text-black">
           {crumbs[crumbs.length - 1]}
         </BreadcrumbPage>
       </BreadcrumbList>
+      ) : (
+        <BreadcrumbList className="text-white">
+          {crumbs.map((crumb:any, index:number) => (
+            crumb === crumbs[crumbs.length - 1] ? null :
+            <BreadcrumbItem key={index}>
+              <BreadcrumbLink href={`/${crumbs.slice(0, index + 1).join('/')}`}>
+                {crumb}
+              </BreadcrumbLink>
+              <BreadcrumbSeparator/>
+            </BreadcrumbItem>
+          ))}
+          <BreadcrumbPage className="text-white">
+            {crumbs[crumbs.length - 1]}
+          </BreadcrumbPage>
+        </BreadcrumbList>
+      )}
     </Breadcrumb>
   )
 }
