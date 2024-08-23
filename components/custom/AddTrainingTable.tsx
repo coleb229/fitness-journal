4e9694f0952dcb5a0d480663d3ea47e2343f6000
Cole Brant <cbrant229@gmail.com>
2024-08-23 03:34:44 -0500

    fixed dialog trigger sizing on add training table component

--- components/custom/AddTrainingTable.tsx
+++ components/custom/AddTrainingTable.tsx
@@ -22,31 +22,33 @@ const exercises = ['benchPress', 'deadlift', 'squat', 'Overhead Press', 'pullUp'
 
 export const AddTrainingTable = () => {
   return (
-    <Dialog>
-      <DialogTrigger>
-        <TriggerComponent />
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            <form action={newTrainingTable}>
-              <Label>Exercise</Label>
-              <ExerciseSelect />
-              <Label>Weight</Label>
-              <Input name="weight" type="number" className="w-1/3" />
-              <button type="submit" className="flex mx-auto my-2 p-4 border-2 hover:bg-black hover:text-white hover:rounded-lg duration-150">Submit</button>
-            </form>
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+    <div className="m-auto">
+      <Dialog>
+        <DialogTrigger >
+          <TriggerComponent />
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              <form action={newTrainingTable}>
+                <Label>Exercise</Label>
+                <ExerciseSelect />
+                <Label>Weight</Label>
+                <Input name="weight" type="number" className="w-1/3" />
+                <button type="submit" className="flex mx-auto my-2 p-4 border-2 hover:bg-black hover:text-white hover:rounded-lg duration-150">Submit</button>
+              </form>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   )
 }
 
 const TriggerComponent = () => {
   return (
-    <div className="w-1/2 bg-white rounded-lg shadow-xl hover:scale-110 duration-150 p-4">
+    <div className="w-[200px] bg-white rounded-lg shadow-xl hover:scale-110 duration-150 p-4">
       <p className="text-xl font-semibold">Add a New Workout to Track</p>
     </div>
   )
